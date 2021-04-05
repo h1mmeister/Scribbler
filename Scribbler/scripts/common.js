8e09c32c47e42790195bad72fcfc641e7e3d16f2
h1m <himanshu.yadav643@gmail.com>
2021-04-05 21:26:31 +0530

    last touches, finishing the project

--- Scribbler/scripts/common.js
+++ Scribbler/scripts/common.js
@@ -1,31 +1,25 @@
-// Search for the modals
+
 var signUpModal = document.getElementById("signUpModal");
 var signInModal = document.getElementById("signInModal");
 
-// Search for the button to open modals
 var signUpBtn  = document.getElementById("signUpBtn");
 var signInBtn  = document.getElementById("signInBtn");
 var signUpLink = document.getElementById("signUpLink");
 
-// Search the button to close modal
 var closeSignUp = document.getElementById("closeSignUp");
 var closeSignIn = document.getElementById("closeSignIn");
 
-//Open the modal
 openModal = modal => {
     modal.style.display = "block";
 };
 
-//Close the modal
 closeModal = modal => {
     modal.style.display = "none";
 };
 
-// Event to handle the click on Signin/signup buttons
 signInBtn.addEventListener("click", () => openModal(signInModal));
 signUpBtn.addEventListener("click", () => openModal(signUpModal));
 
-// Event to handle close button
 closeSignUp.addEventListener("click", () => closeModal(signUpModal));
 closeSignIn.addEventListener("click", () => closeModal(signInModal));
 
@@ -39,8 +33,6 @@ window.addEventListener("click", function(event) {
   }
 });
 
-
-//Handling the link inside the sign in modal
 signUpLink.onclick = function() {
   closeModal(signInModal);
   openModal(signUpModal);
--- Scribbler/scripts/post.js
+++ Scribbler/scripts/post.js
@@ -7,7 +7,6 @@ var titleContent = "Everything you should know about 'module' & 'require' in Nod
 var postsContent = 'Node.js treats each JavaScript file as a separate module. For instance, if you have a file containing some code and this file is named xyz.js, then this file is treated as a module in Node';
 
 window.onload = function() {
-    // Fill in post content
     authorNode.innerHTML = authorContent;
     titleNode.innerHTML = titleContent;
     contentNode.innerHTML = postsContent;
@@ -15,7 +14,6 @@ window.onload = function() {
 
 document.addEventListener("click",function(e) {
     if (hasClass(e.target, "saved")) {
-      // When edit button is clicked
       e.target.classList.remove("saved");
       e.target.classList.add("unsaved");
       e.target.innerHTML = 'Save <i class="fa fa-save"></i>';
@@ -25,7 +23,6 @@ document.addEventListener("click",function(e) {
       contentNode.classList.add("editable");
     }
     else if (hasClass(e.target, "unsaved")) {
-        // When save button is clicked
         e.target.classList.remove("unsaved");
         e.target.classList.add("saved");
         e.target.innerHTML = 'Edit <i class="fa fa-edit"></i>';
@@ -39,7 +36,6 @@ document.addEventListener("click",function(e) {
         sessionStorage.setItem("postContent", contentNode.innerText);
     }
     else if (hasClass(e.target, "like-button")) {
-        // When like button is clicked
         likeCounter++;
         e.target.innerHTML = '<i class="fa fa-thumbs-up"></i> Liked';
         if (likeCounter == 1) {
@@ -51,10 +47,9 @@ document.addEventListener("click",function(e) {
         }
     }
     else if (hasClass(e.target, "comment-button")) {
-        // When Comment button is clicked
         var commentText = document.getElementById("commentBox").value.trim();
         if (commentText !== "") {
-        // Following line of code displays latest comment on the top
+       
           document.getElementById("commentsWrapper").innerHTML = "<p>" +commentText+ "</p>" + document.getElementById("commentsWrapper").innerHTML;
         }        
         document.getElementById("commentBox").value = "";
